15d965e088222b591bc46853e56e386a1c4a277b
davidstern <dstern@fandm.edu>
2017-04-17 14:47:02 -0400

    firebase setup

--- app/src/main/java/com/example/recleague/FindGame.java
+++ app/src/main/java/com/example/recleague/FindGame.java
@@ -11,6 +11,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -24,6 +25,9 @@ public class FindGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_game);
+        gameArray[0] = "hi";
+        gameArray[1] = "yo";
+        gameArray[2] = "hey";
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("game");
@@ -32,8 +36,17 @@ public class FindGame extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
+                ArrayList<String> tmp = dataSnapshot.getValue(t);
+                gameArray = new String[tmp.size()];
+                for (int i =0; i <tmp.size();i++)
+                {
+                    gameArray[i] = tmp.get(i);
+                }
 
-                List<String> value = dataSnapshot.getValue(List.class);
+                //gameArray = tmp.toArray(new String[tmp.size()]);
+                //GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
+                //List<String> value = dataSnapshot.getValue(t);
                 //gameArray = value.toArray(new String[value.size()]);
 
                 //Log.d(TAG, "Value is: " + value);
@@ -51,9 +64,10 @@ public class FindGame extends AppCompatActivity {
         gameArray2[2] = "hey";
 
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameArray2);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameArray);
 
         final ListView listView = (ListView) findViewById(R.id.game_view);
         listView.setAdapter(itemsAdapter);
     }
+
 }
--- app/src/main/java/com/example/recleague/PostGame.java
+++ app/src/main/java/com/example/recleague/PostGame.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -20,128 +21,9 @@ public class PostGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_game);
 
-
-        List<String> games  =  new List<String>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<String> iterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @NonNull
-            @Override
-            public <T> T[] toArray(@NonNull T[] ts) {
-                return null;
-            }
-
-            @Override
-            public boolean add(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends String> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int i, @NonNull Collection<? extends String> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public String get(int i) {
-                return null;
-            }
-
-            @Override
-            public String set(int i, String s) {
-                return null;
-            }
-
-            @Override
-            public void add(int i, String s) {
-
-            }
-
-            @Override
-            public String remove(int i) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<String> listIterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<String> listIterator(int i) {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public List<String> subList(int i, int i1) {
-                return null;
-            }
-        };
+        //GenericTypeIndicator<List<String>> games = new GenericTypeIndicator<List<String>>() {};
+        //List games  =  new List();
+        ArrayList<String> games = new ArrayList<String>();
 
         games.add("g1");
         games.add("g2");
