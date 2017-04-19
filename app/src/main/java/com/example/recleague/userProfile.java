c463404a8b850aedddf2c733b98bda12f2b09a4b
davidstern <dstern@fandm.edu>
2017-04-19 00:35:49 -0400

    user profile

--- app/src/main/java/com/example/recleague/userProfile.java
+++ app/src/main/java/com/example/recleague/userProfile.java
@@ -1,9 +1,57 @@
 package com.example.recleague;
 
+import java.util.ArrayList;
+
 /**
  * Created by davidstern on 4/18/17.
  */
 
 public class userProfile {
 
+    String nickname;
+    String username;
+    String userid;
+    double rating;
+    ArrayList<String> raters;
+    ArrayList<String> joinedgames;
+
+    public userProfile(){
+
+        //needed for firebase
+    }
+
+    public userProfile(String username, String id)
+    {
+        nickname = username;
+        this.username = username;
+        userid =id;
+        rating = 0;
+
+
+
+    }
+
+    public void removeGame(String game)
+    {
+        joinedgames.remove(game);
+    }
+    public void addGame(String game)
+    {
+        joinedgames.add(game);
+    }
+    public ArrayList<String> getJoinedgames()
+    {
+        return joinedgames;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+    public String getNickname()
+    {
+        return nickname;
+    }
 }
