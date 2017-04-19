621cf40aee1dad32ff538784cdb957bf68f664a1
davidstern <dstern@fandm.edu>
2017-04-18 23:04:19 -0400

    firebase setup

--- app/src/main/java/com/example/recleague/FindGame.java
+++ app/src/main/java/com/example/recleague/FindGame.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -25,12 +26,18 @@ public class FindGame extends AppCompatActivity {
 
     String[] gameArray = new String[1];
     ArrayList<gameProfile> masterlist;
+    private String[] sports = new String[3];
+    private String sport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_game);
         gameArray[0] = "Loading...";
+        sports[0] = "soccer";
+        sports[1] = "basketball";
+        sports[2] = "water-polo";
+
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -75,6 +82,34 @@ public class FindGame extends AppCompatActivity {
         });
         update();
 
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        final Spinner s = (Spinner)findViewById(R.id.sports);
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sports);
+
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        s.setAdapter(spinnerArrayAdapter);
+        s.setOnItemSelectedListener(new sportsListener());
+
+
+
+    }
+    class sportsListener implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+            sport = sports[pos];
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            //Do nothing
+        }
     }
 
     public void update()
